56aca051b6eb3cf2e67e0fa613430f63df275099
m.chorakchi@ymail.com <m.chorakchi@ymail.com>
2018-06-27 17:48:42 +0430

    make viewability function

---
+++ script.js
@@ -0,0 +1,35 @@
+/************************************************************************************************
+ *                                                                                              *
+ *                              VARIABLES DECLARATION                                           *
+ *                                                                                              *
+ ************************************************************************************************/
+var adIsViewable = false,
+  viewabilityTime = 0.0,
+  adElement = document.getElementById("ad"),
+  percent;
+
+/**
+ * Logs the viewability values in the console
+ *
+ * @override
+ */
+
+window.log = function () {
+
+  percentage = this.viewability(adElement, document.hasFocus())
+  adIsViewable = percentage > 0  ?  true :  false;
+  viewabilityTime = adIsViewable  ?  viewabilityTime + 0.5 :  viewabilityTime;
+
+  console.clear()
+  console.log("Ad is viewable: ", adIsViewable, "\nViewability time of the ad in sec:", viewabilityTime.toFixed(1), "\nPercentage:", percentage + '%');
+};
+
+
+
+function viewability (element, hasFocus) {
+  var percentX =    (element.offsetWidth-Math.min(element.offsetWidth ,Math.max(0,window.scrollX-element.offsetLeft ))) * 100 /element.offsetWidth
+  var percentY =    (element.offsetHeight-Math.min(element.offsetHeight ,Math.max(0,window.scrollY-element.offsetTop ))) * 100 /element.offsetHeight
+   return hasFocus ? (((percentX * percentY) / 100 ).toFixed()) : 0
+}
+
+
